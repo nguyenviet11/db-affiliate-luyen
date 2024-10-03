339546ffa0fd61997ff4d09e901168bc5078ef6f
viet.nguyen <viet.nguyen2@ntq-solution.com.vn>
2024-10-03 16:59:58 +0700

    update cors

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const Item = require("./model/Item");
 require('dotenv').config()
 const app = express();
 
+app.use(cors({
+  origin: '*', 
+  methods: 'OPTIONS, GET, POST, PUT, PATCH, DELETE',
+  allowedHeaders: 'Content-Type, Authorization, X-Requested-With'
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.get("/status", async (req, res) => {
   const { id } = req.query; 
